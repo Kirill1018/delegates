137e9b8ab3375e61758171f4f68c9050f677d22c
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-03-19 17:04:31 +0300

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Delegate;
+namespace cs
+{
+    public delegate T1 representative<T1, T2>(T1 value, T2 meaning);
+    public class Calculator
+    {
+        public int sum(int value, int meaning)
+        {
+            return value + meaning;
+        }
+        public int subtraction(int value, int meaning)
+        {
+            return value - meaning;
+        }
+        public int multiplication(int value, int meaning)
+        {
+            return value * meaning;
+        }
+        public int division(int value, int meaning)
+        {
+            return value / meaning;
+        }
+    }
+    internal class Delegates
+    {
+        static void Main(string[] args)
+        {
+            representative <int, int> spokesman = null;
+            Calculator calculator = new Calculator();
+            spokesman = calculator.sum;
+            spokesman += calculator.subtraction;
+            spokesman += calculator.multiplication;
+            spokesman += calculator.division;
+            foreach (representative<int, int> item in spokesman.GetInvocationList()) WriteLine($"{item(3, 6)}");
+        }
+    }
+}
